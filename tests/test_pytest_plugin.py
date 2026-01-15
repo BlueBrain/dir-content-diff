6df9b0e72e71c759551bf492137843fba487fe7e
Adrien Berchet <adrien.berchet@gmail.com>
2026-01-15 11:38:14 +0100

    Chore: Bump copyright (#99)

--- tests/test_pytest_plugin.py
+++ tests/test_pytest_plugin.py
@@ -1,7 +1,7 @@
 """Test the pytest plugin."""
 
 # LICENSE HEADER MANAGED BY add-license-header
-# Copyright (c) 2023-2025 Blue Brain Project, EPFL.
+# Copyright (c) 2023-2026 Blue Brain Project, EPFL.
 #
 # This file is part of dir-content-diff.
 # See https://github.com/BlueBrain/dir-content-diff for further info.
